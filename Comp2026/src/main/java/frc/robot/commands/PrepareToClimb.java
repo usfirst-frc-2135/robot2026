4e2ab5dbd60f4f5a8b4da3e9c20deaa74ccda5aa
nishka sethi <nsethi.26@presentationhs.org>
2026-02-19 11:14:10 -0800

    adding intake to PrepareToClimb command

--- Comp2026/src/main/java/frc/robot/commands/PrepareToClimb.java
+++ Comp2026/src/main/java/frc/robot/commands/PrepareToClimb.java
@@ -2,7 +2,9 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants.INConsts;
 import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Intake;
 
 /**
  * Command to acquire a fuel from floor
@@ -14,8 +16,10 @@ public class PrepareToClimb extends SequentialCommandGroup
      * 
      * @param climber
      *            climber subsystem
+     * @param intake
+     *            intake subsystem
      */
-    public PrepareToClimb(Climber climber)
+    public PrepareToClimb(Climber climber, Intake intake)
     {
         setName("PrepareToClimb");
 
@@ -23,8 +27,9 @@ public class PrepareToClimb extends SequentialCommandGroup
                 // Add Commands here: 
 
                 // @formatter:off
-
-        new LogCommand(getName(), "Start Kicker Rollers"), 
+new LogCommand(getName(), "Retract intake "), 
+        intake.getMoveToAngleCommand(INConsts.INRollerMode.STOP, intake::getIntakeRetracted),
+   new LogCommand(getName(), "Extend Climber"), 
         climber.getMoveToPositionCommand(climber::getClimberFullyExtended)
        
         // @formatter:on
