e6b027137bf7279f6e18f5d0ebf207a46a61967f
Jeff Mullins <jeff.l.mullins@gmail.com>
2026-02-19 08:59:55 -0800

    Fix LimelightHelpers package location

--- Comp2026/src/main/java/frc/robot/lib/LimelightHelpers.java
+++ Comp2026/src/main/java/frc/robot/lib/LimelightHelpers.java
@@ -1,14 +1,14 @@
 // LimelightHelpers v1.14 (REQUIRES LLOS 2026.0 OR LATER)
 
-package frc.robot;
+package frc.robot.lib;
 
 import edu.wpi.first.networktables.DoubleArrayEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.TimestampedDoubleArray;
-import frc.robot.LimelightHelpers.LimelightResults;
-import frc.robot.LimelightHelpers.PoseEstimate;
+// import frc.robot.LimelightHelpers.LimelightResults;
+// import frc.robot.LimelightHelpers.PoseEstimate;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
