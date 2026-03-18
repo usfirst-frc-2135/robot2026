54b95443cb8ba7027c983ffe7bbcf0a5735502ea
Iman S <isagarwala.28@presentationhs.org>
2026-03-17 18:03:37 -0700

    dummy paths for command testing

--- Comp2026/src/main/java/frc/robot/autos/AutoScore2.java
+++ Comp2026/src/main/java/frc/robot/autos/AutoScore2.java
@@ -5,8 +5,15 @@ import java.util.List;
 
 import com.pathplanner.lib.path.PathPlannerPath;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.Constants.INConsts;
+import frc.robot.commands.AcquireFuel;
+import frc.robot.commands.LaunchFuel;
 import frc.robot.commands.LogCommand;
+import frc.robot.commands.StopIntaking;
+import frc.robot.commands.StopLaunching;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Hopper;
 import frc.robot.subsystems.Intake;
@@ -38,11 +45,27 @@ public class AutoScore2 extends SequentialCommandGroup
         // @formatter:off
 
         new LogCommand(getName(), "Acquire fuel and score once"),
-        drivetrain.getPathCommand(ppAuto.get(0)),
-        drivetrain.getPathCommand(ppAuto.get(1)),
-        drivetrain.getPathCommand(ppAuto.get(2)),
-        drivetrain.getPathCommand(ppAuto.get(3)),
-        drivetrain.getPathCommand(ppAuto.get(4))
+        new ParallelCommandGroup(
+          new LogCommand(getName(),"Path 1"),
+          drivetrain.getPathCommand(ppAuto.get(0)),
+          new AcquireFuel(intake,hopper)
+          ),
+        new ParallelCommandGroup(
+          new LogCommand(getName(),"Path 2"),
+          drivetrain.getPathCommand(ppAuto.get(1)),
+          launcher.getLauncherScoreCommand()
+          ),
+        new LaunchFuel(hopper, kicker, launcher, intake),
+        new ParallelCommandGroup(
+          new LogCommand(getName(),"Path 3"),
+          drivetrain.getPathCommand(ppAuto.get(2)),
+          new AcquireFuel(intake,hopper)
+          ),
+        new ParallelCommandGroup(
+          drivetrain.getPathCommand(ppAuto.get(3)),
+          launcher.getLauncherScoreCommand()
+          ),
+        new LaunchFuel(hopper, kicker, launcher, intake)
         
         // @formatter:on
     );
