df1942ae5c8a4628046a58e8c386c39e6db876f2
Jeff Mullins <jeff.l.mullins@gmail.com>
2026-03-31 16:20:20 -0700

    Make vision tx and ty be zero when not valid

--- Comp2026/src/main/java/frc/robot/lib/Vision.java
+++ Comp2026/src/main/java/frc/robot/lib/Vision.java
@@ -157,7 +157,9 @@ public class Vision
    */
   public AngularVelocity aimProportional(AngularVelocity maxAngularRate)
   {
-    double proportionalFactor = -(LimelightHelpers.getTX(Constants.kLLFrontName) * kTXOffset) * kAimingKp;
+    double tx =
+        LimelightHelpers.getTV(Constants.kLLFrontName) ? (LimelightHelpers.getTX(Constants.kLLFrontName) + kTXOffset) : 0.0;
+    double proportionalFactor = -tx * kAimingKp;
 
     return maxAngularRate.times(proportionalFactor);
   }
@@ -172,7 +174,9 @@ public class Vision
    */
   public LinearVelocity rangeProportional(LinearVelocity maxSpeed)
   {
-    double proportionalFactor = -(LimelightHelpers.getTY(Constants.kLLFrontName) + kTYOffset) * kDrivingKp;
+    double ty =
+        LimelightHelpers.getTV(Constants.kLLFrontName) ? (LimelightHelpers.getTY(Constants.kLLFrontName) + kTYOffset) : 0.0;
+    double proportionalFactor = -ty * kDrivingKp;
 
     return maxSpeed.times(proportionalFactor);
   }
